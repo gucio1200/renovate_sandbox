8cc58c5cc0375ea9f07494c5b6c789b5496f2f16
gucio1200 <kacper.gogol@gmail.com>
2025-02-17 10:22:36 +0100

    Add renovate for locales

--- tf/locals.tf
+++ tf/locals.tf
@@ -6,11 +6,18 @@ locals {
   client-id                = data.azurerm_client_config.current.client_id
   tenant-id                = data.azurerm_client_config.current.tenant_id
 
+  # renovate: depName=argoproj/argo-cd datasource=github-releases
   argocd_version           = "7.7.10"
+  # renovate: depName=external-secrets/external-secrets datasource=github-releases
   external_secrets_version = "0.11.0"
+  # renovate: depName=jetstack/trust-manager datasource=github-releases
   trust_manager_version    = "v0.14.0"
+  # renovate: depName=jetstack/cert-manager datasource=github-releases
   cert_manager_version     = "v1.16.2"
+  # renovate: depName=linkerd/linkerd2 datasource=github-releases
   linkerd-crds_version     = "1.8.0"
+  # renovate: depName=linkerd/linkerd2 datasource=github-releases
   linkerd_version          = "1.16.11"
+  # renovate: depName=traefik/traefik datasource=github-releases
   traefik_version          = "33.2.1"
 }
