ce6b081a9e91b1b3d7197d8ba22d8863e5f6c9bd
gucio1200 <kacper.gogol@gmail.com>
2025-03-12 21:56:06 +0100

    Ok...

--- generate_matrix.sh
+++ generate_matrix.sh
@@ -4,7 +4,6 @@
 set -e
 
 # Read environment variables
-DIR_DEPTH="${DIR_DEPTH}"  # Specifies how deep to search in the directory structure
 INPUT_LIST=(${CHANGED_FILES})    # List of changed files from environment variables, converted to an array
 
 # Initialize the JSON output with an empty 'include' array
@@ -14,14 +13,19 @@ OUTPUT='{"include": []}'
 for path in "${INPUT_LIST[@]}"; do
   # Determine the depth of the current file path based on the number of '/' characters
   DEPTH=$(echo "$path" | awk -F'/' '{print NF}')
+  REQUIRED_DEPTH=$((3 - DEPTH))
 
   # Find subfolders at the required depth relative to the specified directory depth
-  SUBFOLDERS=$(find "$path" -type d -mindepth $((DIR_DEPTH - DEPTH)) -maxdepth $((DIR_DEPTH - DEPTH)) 2>/dev/null || true)
+  if [[ $REQUIRED_DEPTH -ge 0 ]]; then
+    SUBFOLDERS=$(find "$path" -type d -mindepth $REQUIRED_DEPTH -maxdepth $REQUIRED_DEPTH 2>/dev/null || true)
+  else
+    SUBFOLDERS="$path"
+  fi
 
   # Loop through each discovered subfolder
   for folder in $SUBFOLDERS; do
     # Extract workspace, region, and cluster names by splitting the folder path
-    IFS='/' read -r workspace region cluster <<< "$folder"
+    IFS='/' read -r workspace region cluster _ <<< "$folder"
 
     # Append extracted values to the JSON output using jq
     OUTPUT=$(echo "$OUTPUT" | jq --arg workspace "$workspace" --arg region "$region" --arg cluster "$cluster" \
